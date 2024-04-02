3500bed0038b266b9c7d27782b6ed57fb8b44fd4
Mac Cody <maccody@att.net>
2024-04-01 20:57:21 -0500

    Added GPL 3 license header to file.
    Added GPL 3 license header to files unique to KanaChord Keyboard software.

--- software/kbd_mgt.cpp
+++ software/kbd_mgt.cpp
@@ -1,4 +1,21 @@
 /* kbd_mgt.cpp */
+/**
+This file is part of KanaChord Keyboard.
+ Copyright (C) 2024 Mac A. Cody
+
+ KanaChord Keyboard is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+**/
 
 #include <Arduino.h>
 
@@ -7,6 +24,8 @@
 #include <Adafruit_NeoPixel.h>
 #include "kana.h"
 #include "kbd_mgt.h"
+
+//#define DEBUG 1
 
 /*
  * The Adafruit Neokey board, which has five rows and six columns,
@@ -324,6 +343,7 @@ uint16_t kbd_process(void) {
   
   return(kanaValue);
 }
+
 
 #define HIRAGANA false
 #define KATAKANA true
